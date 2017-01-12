2a3a447cdf6bd45a263da8f49e51241bdcffe154
Damien Desnoe <damien.desnoe@gmail.com>
2017-01-12 16:34:36 +0100

    Add "Lang options" feature + correct nav styles

--- js/lang.js
+++ js/lang.js
@@ -1,6 +1,32 @@
-var bodyElement = document.querySelector('body');
-if (bodyElement.lang === 'fr') {
-	
-} else {
-	document.write('false');
+var titleElement = document.querySelector('title');
+var frTitle = 'A propos de moi - Damien Desnoë';
+var enTitle = titleElement.textContent;
+var articleElement = document.querySelector('article');
+var enProfileElement = document.getElementById('profile-en');
+var frProfileElement = document.getElementById('profile-fr');
+var enOption = document.getElementsByTagName('li')[0];
+var frOption = document.getElementsByTagName('li')[1];
+
+changeProfileLanguage(enOption);
+
+// This function change the language of the page without the need to reload it
+function changeProfileLanguage(e) {
+
+	if (e === enOption) {
+		frOption.classList.remove('active');
+		e.classList.add('active');
+		
+		titleElement.textContent = enTitle;
+
+		articleElement.removeChild(frProfileElement);
+		articleElement.appendChild(enProfileElement);
+	} else {
+		enOption.classList.remove('active');
+		e.classList.add('active');
+		
+		titleElement.textContent = frTitle;
+		
+		articleElement.removeChild(enProfileElement);
+		articleElement.appendChild(frProfileElement);
+	}
 }
